e2f4fbe4fd607c47a8edc5bfdffe5a62be49dd07
k4kumar <kumarjitdutta581@gmail.com>
2020-07-25 18:38:18 +0600

    initCommit

---
+++ app/src/main/java/com/bongobondhuparishad/bloodbank/MainActivity.java
@@ -0,0 +1,59 @@
+package com.bongobondhuparishad.bloodbank;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity  {
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
+    NavigationView navigationView;
+    LinearLayout add_donor,donor_list;
+    RelativeLayout adminRel,donorListRel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view1);
+
+        setToolbar();
+    }
+
+
+    private void setToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setTitle("");
+
+        toolbar.findViewById(R.id.navigation_menu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.e("Click", "keryu");
+
+                if (drawer.isDrawerOpen(navigationView)) {
+                    drawer.closeDrawer(navigationView);
+                } else {
+                    drawer.openDrawer(navigationView);
+                }
+            }
+        });
+
+    }
+
+}
